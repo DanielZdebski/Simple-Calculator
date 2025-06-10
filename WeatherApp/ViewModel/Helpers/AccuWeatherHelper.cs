847bd971e484a33a642ca437fd1ab27619ed6924
Daniel <Daniel.Zdebski@gmail.com>
2025-06-10 17:43:00 +0100

    This has a progress on MVVM

---
+++ WeatherApp/ViewModel/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel.Helpers
+{
+    public class AccuWeatherHelper
+    {
+        public const string api_key = "jBRKohc78gQa9c5eNPfEshGoGlM9jXKv";
+        public const string base_url = "http://dataservice.accuweather.com/";
+
+        #region Endpoints
+        public const string autoComplete_endpoint = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
+        public const string currentConditions_endpoint = "currentconditions/v1/{0}?apikey={1}";
+        #endregion
+
+        #region Query methods
+        public static async Task<List<City>?> GetCities(string query)
+        {
+            List<City>? cities = new List<City>();
+
+            string url = base_url + string.Format(autoComplete_endpoint, api_key, query);
+
+            using (HttpClient client = new HttpClient()) 
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+
+                cities = JsonConvert.DeserializeObject<List<City>>(json);
+            }
+
+            return cities;
+        }
+
+        public static async Task<CurrentConditions?> GetCurrentConditions(string cityKey)
+        {
+            CurrentConditions? currentConditions = new CurrentConditions();
+
+            string url = base_url + string.Format(currentConditions_endpoint, cityKey, api_key);
+
+            using (HttpClient client = new HttpClient()) 
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+
+                currentConditions = (JsonConvert.DeserializeObject<List<CurrentConditions>>(json)).FirstOrDefault();
+            }
+
+            return currentConditions;
+        }
+        #endregion
+    }
+}
---
+++ WeatherApp/ViewModel/WeatherVM.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+
+        public WeatherVM()
+        { 
+            // This code is only for DesignMode to view the values.
+            if(DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                SelectedCity = new City();
+                SelectedCity.LocalizedName = "New York";
+
+                CurrentConditions = new CurrentConditions
+                {
+                    WeatherText = "Partly cloudy",
+                    Temperature = new Temperature
+                    {
+                        Metric = new Units
+                        {
+                            Value = 21
+                        }
+                    }
+                };
+            }
+        }
+
+        #region Query property
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set 
+            {
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+        #endregion
+
+        #region CurrentConditions property
+        private CurrentConditions currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set 
+            { 
+                currentConditions = value;
+                OnPropertyChanged("CurrentConditions");
+            }
+        }
+        #endregion
+
+        #region SelectedCity property
+        private City selectedCity;
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set 
+            { 
+                selectedCity = value;
+
+                OnPropertyChanged("SelectedCity");
+            }
+        }
+        #endregion
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

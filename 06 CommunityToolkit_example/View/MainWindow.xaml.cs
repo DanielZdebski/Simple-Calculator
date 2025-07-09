60ed1365d310b93fc54f941e86d8ed296e0e1d3e
Daniel <Daniel.Zdebski@gmail.com>
2025-07-09 12:15:34 +0100

    this has CommunityToolkit example

---
+++ 06 CommunityToolkit_example/View/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using _06_CommunityToolkit_example.ViewModel;
+
+namespace _06_CommunityToolkit_example.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+//        private MainWindowViewModel ViewModel => (MainWindowViewModel)ViewModel;
+        public MainWindow()
+        {
+//            DataContext = ViewModel = new MainWindowViewModel();
+            DataContext = new MainWindowViewModel();
+            InitializeComponent();
+        }
+    }
+}
---
+++ 06 CommunityToolkit_example/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,49 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using System.Windows.Input;
+
+namespace _06_CommunityToolkit_example.ViewModel
+{
+    public record class userLoggedIn(string UserName);
+
+    [ObservableObject]
+    public partial class MainWindowViewModel
+    {
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(ClickCommand))]
+        private string? firstName = "Daniel";
+
+        public MainWindowViewModel()
+        {
+        }
+
+        [RelayCommand(IncludeCancelCommand = true)]
+        private async Task Click(CancellationToken token)
+        {
+            try { 
+                await Task.Delay(5_000, token);
+                FirstName += " Robert";
+            }
+            catch(OperationCanceledException)
+            {
+                FirstName += " x";
+            }
+        }
+
+        private bool CanClick() => FirstName == "Daniel";
+
+        public void Receive(userLoggedIn message)
+        {
+            
+        }
+    }
+
+}

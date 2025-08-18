3fd1ea3e5493f357d33fb81631886400f5127546
Daniel <Daniel.Zdebski@gmail.com>
2025-08-18 13:45:01 +0100

    progress on ASP.Net (doesn't work the build)

---
+++ DiaryApp/Models/DiaryEntry.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics.Metrics;
+using System.Runtime.Intrinsics.X86;
+
+namespace DiaryApp.Models
+{
+    public class DiaryEntry
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        
+        [Required]
+        public string Content { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime Created {  get; set; } = DateTime.Now;
+    }
+}
+// FSE-3GFPHR3\SQLEXPRESS; initial catalog = master; trusted_connection = true
